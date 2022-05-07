e5d02fbd079a8c5f6542ba19ae27469a85506521
ByxoiPetrovi4 <zames96@gmail.com>
2022-05-07 23:32:20 +0300

    Added serial read functional, scrollbar fixed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     serialDialog = new SerialDiag(this);
     rabbitDialog = new RabbitDiag(this);
+    serialPort = new QSerialPort(this);
+
+    connected = false;
+
+    connect(serialPort, &QSerialPort::readyRead, this, &MainWindow::readData);
+
 }
 
 MainWindow::~MainWindow()
@@ -15,6 +21,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::showStatusMessage(const QString &str)
+{
+    ui->connectStatus->setText(str);
+}
+
+void MainWindow::readData()
+{
+    const QString str(serialPort->readAll());
+    ui->plainTextEdit->moveCursor(QTextCursor::End);
+    ui->plainTextEdit->insertPlainText(str);
+}
 
 void MainWindow::on_actionSerial_settings_triggered()
 {
@@ -25,5 +42,41 @@ void MainWindow::on_actionSerial_settings_triggered()
 void MainWindow::on_actionRecord_settings_triggered()
 {
     rabbitDialog->show();
+
+}
+
+
+void MainWindow::on_connectButton_clicked()
+{
+    if(connected)
+    {
+        if(serialPort->disconnect())
+        {
+            showStatusMessage(tr("Disconnected"));
+            connected = false;
+            ui->connectButton->setText(tr("Connect"));
+            return;
+        }
+        QMessageBox::critical(this, tr("Error"), serialPort->errorString());
+        showStatusMessage(tr("Serial ERROR!"));
+    }
+    const SerialDiag::Settings p = serialDialog->settings();
+    serialPort->setPortName(p.name);
+    serialPort->setBaudRate(p.baudRate);
+    serialPort->setDataBits(p.dataBits);
+    serialPort->setParity(p.parity);
+    serialPort->setStopBits(p.stopBits);
+    serialPort->setFlowControl(p.flowControl);
+    if (serialPort->open(QIODevice::ReadWrite)) {
+        connected = true;
+        ui->connectButton->setText(tr("Disconnect"));
+        showStatusMessage(tr("Connected to %1 : %2, %3, %4, %5, %6")
+                          .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                          .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+    } else {
+        QMessageBox::critical(this, tr("Error"), serialPort->errorString());
+
+        showStatusMessage(tr("Open error"));
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QSerialPort>
+#include <QMessageBox>
 #include "serialdiag.h"
 #include "rabbitdiag.h"
 
@@ -16,15 +19,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void showStatusMessage(const QString &);
 
 private slots:
+    void readData();
+
     void on_actionSerial_settings_triggered();
 
     void on_actionRecord_settings_triggered();
 
+    void on_connectButton_clicked();
+
 private:
-    Ui::MainWindow *ui;
-    SerialDiag*     serialDialog;
-    RabbitDiag*     rabbitDialog;
+    Ui::MainWindow *ui = nullptr;
+    SerialDiag*     serialDialog = nullptr;
+    RabbitDiag*     rabbitDialog = nullptr;
+    QSerialPort*    serialPort = nullptr;
+
+    bool connected;
 };
 #endif // MAINWINDOW_H
